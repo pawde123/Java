abc06367926d4fe8d7378aba5f190382ba0f6e0a
pawde123 <shivaraj364@gmail.com>
2020-03-10 20:21:09 +0530

    java basics

---
+++ src/basics/Basics.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int count=0;count<10;count++)
+		{
+			System.out.println("print line"+count);
+			count--;
+			System.out.println("value after decrement"+count);
+		}
+	}
+
+}
---
+++ src/basics/Basics1.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0,j=0,k=0;
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i);
+		System.out.println(j--);
+		System.out.println(j);
+		System.out.println(--j);
+		System.out.println(j);
+		int x=k++;
+		System.out.println(x+"   "+k++);
+		System.out.println(k--);
+
+	}
+
+}
